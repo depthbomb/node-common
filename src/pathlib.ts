0197cafd6a513f585856ea87bf1f16c78cd1f956
depthbomb <depthbomb@users.noreply.github.com>
2026-03-26 11:40:22 -0500

    chore: update dependencies

--- src/pathlib.ts
+++ src/pathlib.ts
@@ -1,6 +1,6 @@
 import * as os from 'node:os';
-import * as path from 'node:path';
 import * as fsSync from 'node:fs';
+import * as path from 'node:path';
 import * as fs from 'node:fs/promises';
 import * as readline from 'node:readline';
 import { fileURLToPath, pathToFileURL } from 'node:url';
